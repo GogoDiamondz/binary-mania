de7e45cc18abf2a0cada9786c1bc0d619fd466a6
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-05 16:44:16 -0700

    friends jsx

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import './friends.css';
 
 export function Friends() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>friends displayed here</div>
+      <main>
+        <h1>Your Friends</h1>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Your Wins</th>
+                    <th>Friend's Wins</th>
+                    <th>Play</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>[friend's name]</td>
+                    <td>[your wins]</td>
+                    <td>[friend's wins]</td>
+                    <td><button>Play</button></td>
+                </tr>
+                <tr>
+                    <td>[friend's name]</td>
+                    <td>[your wins]</td>
+                    <td>[friend's wins]</td>
+                    <td><button>Play</button></td>
+                </tr>
+                <tr>
+                    <td>[friend's name]</td>
+                    <td>[your wins]</td>
+                    <td>[friend's wins]</td>
+                    <td><button>Play</button></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2>Players Online</h2>
+        <table className="table">
+            <thead>
+            <tr>
+                <th>Player Name</th>
+                <th>Friend Request</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+            <tr>
+                <td>[player name]</td>
+                <td><button>Add Friend</button></td>
+            </tr>
+        </tbody>
+        </table>
     </main>
   );
 }
