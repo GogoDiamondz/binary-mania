1e995d0ba94a8482a04df27aa067430944c7cbd9
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-05 16:30:35 -0700

    create jsx files

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+            <header>
+                <h1>Binary Mania</h1>
+                <nav>
+                    <div><a href="index.html">Home</a></div>
+                    <div><a href="play.html">Play</a></div>
+                    <div><a href="friends.html">Friends</a></div>
+                    <div><a href="profile.html">Profile</a></div>
+                </nav>
+            </header>
+            
+            <main>
+                App components go here
+            </main>
+
+            <footer>
+                <span className="text-reset">Hannah Heydorn</span>
+                <a href="https://github.com/GogoDiamondz/binary-mania">GitHub</a>
+            </footer>
+        </div>;
 }
