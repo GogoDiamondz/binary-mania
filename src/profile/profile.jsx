20e36e10785cb05fe56a173b51fb955ddf0aee2d
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-05 17:32:18 -0700

    profile jsx

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './profile.css';
 
 export function Profile() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>profile displayed here</div>
-    </main>
+      <main className="profile-main">
+      <h1>[Username]'s Profile</h1>
+      <p>Best Time: [best time]</p>
+      {/* Random image of a duck, replace with 3rd party API duck image */}
+      <img src="duck.jpg" alt="duck" width="400px" />
+      <button id="logout">Log Out</button>
+  </main>
   );
 }
