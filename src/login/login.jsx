3b8acdb1748f4e1bc8f9e48e17b50d2ef73c4e12
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-05 17:23:42 -0700

    specify login main styling

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="login-main">
+      <div className="login-box">
+          <h1>Welcome to Binary Mania</h1>
+          <form method="get" action="play.html">
+              <div className="form-group">
+                  <input type="text" placeholder="username"></input>
+              </div>
+              <div>
+                  <input type="password" placeholder="password"></input>
+              </div>
+              <button type="submit">Play</button>
+          </form>
+      </div>
     </main>
   );
 }
