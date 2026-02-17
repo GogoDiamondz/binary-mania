7cd631d17fed29da89f098882e696e11aab82a1a
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-16 21:57:11 -0700

    store userName in localStorage

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './login.css';
 
-export function Login() {
+export function Login(props) {
+  const [userName, setUserName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  async function loginUser() {
+    localStorage.setItem('userName', userName);
+  }
+
   return (
     <main className="login-main">
       <div className="login-box">
-          <h1>Welcome to Binary Mania</h1>
-          <form method="get" action="play.html">
-              <div className="form-group">
-                  <input type="text" placeholder="username"></input>
-              </div>
-              <div>
-                  <input type="password" placeholder="password"></input>
-              </div>
-              <button type="submit">Play</button>
-          </form>
+        <h1>Welcome to Binary Mania</h1>
+        <div>
+          <input type="text" placeholder="username" value={userName} onChange={(e) => setUserName(e.target.value)} />
+        </div>
+        <div>
+            <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit" onClick={() => loginUser()} disabled={!userName || !password}>Login</button>
       </div>
     </main>
   );
