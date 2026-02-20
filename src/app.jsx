44302253cfdde41c55d8699ffc6e0b4bd46fd324
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-20 15:53:40 -0700

    added authState class

--- src/app.jsx
+++ src/app.jsx
@@ -7,8 +7,13 @@ import { Play } from './play/play';
 import { Friends } from './friends/friends';
 import { Profile } from './profile/profile';
 import { Login } from './login/login';
+import { AuthState } from './login/authState';
 
 export default function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
     return (
     <BrowserRouter>
         <div className="body bg-dark text-light">
@@ -16,14 +21,28 @@ export default function App() {
                 <h1>Binary Mania</h1>
                 <nav>
                     <div><NavLink to="/">Home</NavLink></div>
-                    <div><NavLink to="/play">Play</NavLink></div>
-                    <div><NavLink to="/friends">Friends</NavLink></div>
+                    {authState === AuthState.Authenticated && (
+                        <div><NavLink to="/play">Play</NavLink></div>
+                    )}
+                    {authState === AuthState.Authenticated && (
+                        <div><NavLink to="/friends">Friends</NavLink></div>
+                    )}
+                    {/* TODO: replace 'home' with 'profile' once logged in */}
                     <div><NavLink to="/profile">Profile</NavLink></div>
                 </nav>
             </header>
             
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={
+                    <Login 
+                        userName={userName}
+                        authState={authState}
+                        onAuthChange={(userName, authState) => {
+                            setAuthState(authState);
+                            setUserName(userName);
+                        }}
+                    />
+                    } />
                 <Route path="/play" element={<Play />} />
                 <Route path="/friends" element={<Friends />} />
                 <Route path="/profile" element={<Profile />} />
---
+++ src/login/authState.js
@@ -0,0 +1,9 @@
+export class AuthState {
+  static Unknown = new AuthState('unknown');
+  static Authenticated = new AuthState('authenticated');
+  static Unauthenticated = new AuthState('unauthenticated');
+
+  constructor(state) {
+    this.state = state;
+  }
+}
