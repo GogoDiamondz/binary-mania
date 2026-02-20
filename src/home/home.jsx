c31559de07cedf978f5313fd2e0840afc88adf5c
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-20 16:47:10 -0700

    finish routing from home

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,3 +1,33 @@
-export function Home() {
-    
+import React from 'react';
+
+import { Login } from './login';
+import { Profile } from './profile';
+import { AuthState } from './authState';
+
+export function Home({ userName, authState, onAuthChange }) {
+    return (
+        <main className="home-main">
+            <div>
+                {authState === AuthState.Unknown && (
+                    <h1>Welcome to Binary Mania</h1>
+                )}
+                {authState === AuthState.Unauthenticated && (
+                    <Login 
+                        userName={userName}
+                        onLogin={(inputUserName) => {
+                            onAuthChange(inputUserName, AuthState.Authenticated)
+                        }}
+                    />
+                )}
+                {authState === AuthState.Authenticated && (
+                    <Profile 
+                        userName={userName} 
+                        onLogOut={() => {
+                            onAuthChange('', AuthState.Unauthenticated);
+                        }}
+                    />
+                )}
+            </div>
+        </main>
+    );
 }
