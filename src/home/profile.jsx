8ca9e37fae9c9a0b12f99301f28995f379cf8806
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-02-26 17:43:13 -0700

    profile play and friends buttons

--- src/home/profile.jsx
+++ src/home/profile.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 import './profile.css';
 
 export function Profile(props) {
+  const navigate = useNavigate();
+
+  function clickPlay() {
+    navigate("/play");
+  }
+
+  function clickFriends() {
+    navigate("/friends");
+  }
 
   function logout() {
     localStorage.removeItem('userName');
@@ -15,6 +25,10 @@ export function Profile(props) {
       <p>Best Time: [best time]</p>
       {/* Random image of a duck, replace with 3rd party API duck image */}
       <img src="duck.jpg" alt="duck" width="400px" />
+      <div className="navigation-buttons">
+        <button id="play" onClick={() => clickPlay()}>Play</button>
+        <button id="friends" onClick={() => clickFriends()}>Friends</button>
+      </div>
       <button id="logout" onClick={() => logout()}>Log Out</button>
   </main>
   );
