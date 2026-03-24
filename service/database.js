5a61ec54036f0f1356159747a9930027401cc00f
Hannah Heydorn <hannah.heydorn@gmail.com>
2026-03-24 11:15:00 -0600

    connected to DB

---
+++ service/database.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('mania');
+const userCollection = db.collection('users');
+const onlineCollection = db.collection('onlinePlayers');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`DB connected to ${config.hostname}`);
+  }
+  catch (err) {
+    console.error('Failed to connect to database', err);
+    process.exit(1);
+  }
+})();
